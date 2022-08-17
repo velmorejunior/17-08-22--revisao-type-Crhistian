784e79715d213a7d34f527719fc60c9f879997af
RichardWillianx <richardwilliansp@gmail.com>
2022-08-17 20:00:40 -0300

    Function CadastrarProduto

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,36 @@
 "use strict";
 //Cadastrar, listar, excluir
-function cadastrarProduto() {
+let codigoAtual = 0;
+function cadastrarProduto(nome, preco, tipo, freteGratis, coresDisponiveis) {
+    let novoProduto = {
+        codigo: codigoAtual,
+        nome,
+        preco,
+        tipo,
+        quantidade: 0,
+        freteGratis,
+        coresDisponiveis,
+    };
+    codigoAtual++;
+    return novoProduto;
 }
+// console.log(
+//   cadastrarProduto("Pente", 10, "utensilio", true, [
+//     "azul",
+//     "verde",
+//     "vermelho",
+//   ])
+// );
+const listaProdutos = [];
+listaProdutos.push(cadastrarProduto("Pente", 10, "utensilio", true, [
+    "azul",
+    "verde",
+    "vermelho",
+]));
+listaProdutos.push(cadastrarProduto("Escova de Dente", 15, "Higiene Bucal", true, [
+    "preta",
+    "branca",
+    "roxa",
+]));
+listaProdutos.push(cadastrarProduto("Cubo mágico", 20, "Brinquedo", false, []));
+console.log(listaProdutos);
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,65 @@
 //Cadastrar, listar, excluir
 
-type Produto= {
-    codigo: number,
-    nome: string,
-    descricao: string,
-    preco: number,
-    tipo: string,
-    quantidade: number,
-    freteGratis: boolean,
-    coresDisponiveis: Array<string>
+type Produto = {
+  codigo: number;
+  nome: string;
+  descricao?: string;
+  preco: number;
+  tipo: string;
+  quantidade: number;
+  freteGratis: boolean;
+  coresDisponiveis: Array<string>;
+};
+
+let codigoAtual: number = 0;
+
+function cadastrarProduto(
+  nome: string,
+  preco: number,
+  tipo: string,
+  freteGratis: boolean,
+  coresDisponiveis: Array<string>
+): Produto {
+  let novoProduto: Produto = {
+    codigo: codigoAtual,
+    nome,
+    preco,
+    tipo,
+    quantidade: 0,
+    freteGratis,
+    coresDisponiveis,
+  };
+
+  codigoAtual++;
+
+  return novoProduto;
 }
 
-function cadastrarProduto():Produto{
-    
-}
+// console.log(
+//   cadastrarProduto("Pente", 10, "utensilio", true, [
+//     "azul",
+//     "verde",
+//     "vermelho",
+//   ])
+// );
+
+const listaProdutos: Array<Produto> = [];
+
+listaProdutos.push(
+  cadastrarProduto("Pente", 10, "utensilio", true, [
+    "azul",
+    "verde",
+    "vermelho",
+  ])
+);
+listaProdutos.push(
+  cadastrarProduto("Escova de Dente", 15, "Higiene Bucal", true, [
+    "preta",
+    "branca",
+    "roxa",
+  ])
+);
+
+listaProdutos.push(cadastrarProduto("Cubo mágico", 20, "Brinquedo", false, []));
+
+console.log(listaProdutos);
